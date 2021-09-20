5221c7b33c7180da3f702af6c11f7bd58af7b0a1
Amer Bilic <bilic.amer@outlook.com>
2021-09-21 01:26:55 +0200

    prisma

--- client/src/store/Addresses/addresses-actions.js
+++ client/src/store/Addresses/addresses-actions.js
@@ -8,5 +8,7 @@ import toast from "react-hot-toast";
 
 export const fetchUserAddresses = (userId) => async (dispatch) => {
   try {
+    const res = await axios.get(`address/${userId}`);
+    
   } catch (error) {}
 };
